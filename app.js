a6536c01b36a33d6c2f65013073c11f7672adc10
Cyril Marois <cyrilmarois@gmail.com>
2023-10-20 17:55:09 +0200

    feat(counter): move logic in app.js

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
 });
 
 app.mount('#events');
