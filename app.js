6504d1211b9bc8ded1fd0997079bdaf9426ed728
Cyril Marois <cyrilmarois@gmail.com>
2023-10-20 13:40:22 +0200

    feat(app): add link and method

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
   data() {
+    // same as data: function() {}
     return {
       courseGoal: 'Finish the course and learn Vue!',
+      vueLink: 'https://vue.js.org',
     };
+  },
+
+  methods: {
+    outputGoals() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    },
   },
 });
 
