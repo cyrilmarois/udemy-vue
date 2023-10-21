85fa33aa40e86887cbc86b37a5ee785f720d813e
Cyril Marois <cyrilmarois@gmail.com>
2023-10-21 13:30:23 +0200

    feat(input): display full name using method

--- 03-basic-native-event-object/app.js
+++ 03-basic-native-event-object/app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'HEROOO';
+    },
+
     setName(event, lastName) {
       this.name = event.target.value;
     },
