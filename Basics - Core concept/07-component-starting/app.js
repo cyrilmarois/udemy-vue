668e1b2fc80b6f7c9474b4c762c3aa8c1c888996
Cyril Marois <cyrilmarois@gmail.com>
2023-10-23 19:09:19 +0200

    feat(cmpt): register contact-list cpmt

--- Basics - Core concept/07-component-starting/app.js
+++ Basics - Core concept/07-component-starting/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      toggleContactInfo: true,
       contacts: [
         {
           id: null,
@@ -21,11 +20,37 @@ const app = Vue.createApp({
         : 'Hide');
     },
   },
+  methods: {},
+});
+app.component('contact-list', {
+  template: `<li :key="contact.id">
+          <h2>{{ contact.firstName }} {{ contact.lastName }}</h2>
+          <button @click="toggleContactDetails">
+            Show Details
+          </button>
+          <ul v-if="toggleContactInfo">
+            <li><strong>Phone:</strong> {{contact.phone }}</li>
+            <li><strong>Email:</strong> {{ contact.email }}</li>
+          </ul>
+        </li>`,
+  data() {
+    return {
+      contact: {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        phone: '01 02 03 04 05',
+        email: 'jefaismestests@gomail.com',
+      },
+      toggleContactInfo: true,
+
+      isContactDetailsVisible: true,
+    };
+  },
   methods: {
     toggleContactDetails() {
       this.toggleContactInfo = !this.toggleContactInfo;
     },
   },
 });
-
 app.mount('#app');
