ac992d75f334e5cc701694112b6f1977aa987f46
Cyril Marois <cyrilmarois@gmail.com>
2023-10-23 10:24:01 +0200

    feat(attack): special attack for player

--- projects/monster/app.js
+++ projects/monster/app.js
@@ -35,6 +35,11 @@ const app = Vue.createApp({
       const damage = getRandomValue(8, 15); // hit between 8 and 15
       this.playerHealth -= damage;
     },
+    specialAttack() {
+      const damage = getRandomValue(10, 25);
+      this.monsterHealth -= damage;
+      this.attackPlayer();
+    },
   },
 });
 
