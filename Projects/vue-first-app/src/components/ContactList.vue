369084b85ecedeb8d12c781989de2971ad58021a
Cyril Marois <cyrilmarois@gmail.com>
2023-10-23 23:43:53 +0200

    feat(contact): output contact component

---
+++ Projects/vue-first-app/src/components/ContactList.vue
@@ -0,0 +1,32 @@
+<template>
+  <li :key="contact.id">
+    <h2>{{ contact.firstName }} {{ contact.lastName }}</h2>
+    <button @click="toggleContactDetails">Show Details</button>
+    <ul v-if="toggleContactInfo">
+      <li><strong>Phone:</strong> {{ contact.phone }}</li>
+      <li><strong>Email:</strong> {{ contact.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggleContactInfo: true,
+      contact: {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        phone: '01 02 03 04 05',
+        email: 'jefaismestests@gomail.com',
+      },
+    };
+  },
+  methods: {
+    toggleContactDetails() {
+      this.toggleContactInfo = !this.toggleContactInfo;
+    },
+  },
+};
+</script>
