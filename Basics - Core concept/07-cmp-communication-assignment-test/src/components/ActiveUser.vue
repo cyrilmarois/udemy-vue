2fa6ab7f1e83708e8a7fcd5aabc7c9978a8fab10
Cyril Marois <cyrilmarois@gmail.com>
2023-10-24 16:19:50 +0200

    feat(test): test solving

---
+++ Basics - Core concept/07-cmp-communication-assignment-test/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ personName }}</h2>
+    <h3>{{ personAge }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    personName: {
+      type: String,
+      required: true,
+    },
+    personAge: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
