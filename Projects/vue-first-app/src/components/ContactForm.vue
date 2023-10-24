fcf62fb54938b6ba7d6fa9679679e4ed23f72463
Cyril Marois <cyrilmarois@gmail.com>
2023-10-24 14:46:01 +0200

    style(form): put form into list

--- Projects/vue-first-app/src/components/ContactForm.vue
+++ Projects/vue-first-app/src/components/ContactForm.vue
@@ -1,23 +1,25 @@
 <template>
-  <form @submit.prevent="addContact">
-    <div>
-      <label for="firstName"> First Name</label>
-      <input type="text" v-model="firstName" />
-    </div>
-    <div>
-      <label for="lastName"> Last Name</label>
-      <input type="text" v-model="lastName" />
-    </div>
-    <div>
-      <label for="phone">Phone</label>
-      <input type="phone" v-model="phone" />
-    </div>
-    <div>
-      <label for="email">Email</label>
-      <input type="email" v-model="email" />
-    </div>
-    <button type="submit">Register</button>
-  </form>
+  <li>
+    <form @submit.prevent="addContact">
+      <div>
+        <label for="firstName"> First Name</label>
+        <input type="text" v-model="firstName" />
+      </div>
+      <div>
+        <label for="lastName"> Last Name</label>
+        <input type="text" v-model="lastName" />
+      </div>
+      <div>
+        <label for="phone">Phone</label>
+        <input type="phone" v-model="phone" />
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input type="email" v-model="email" />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </li>
 </template>
 
 <style>
