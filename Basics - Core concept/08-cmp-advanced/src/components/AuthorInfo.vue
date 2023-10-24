1b3451c6e71c709a34f70862418bda0e7389b5cc
Cyril Marois <cyrilmarois@gmail.com>
2023-10-25 00:06:06 +0200

    feat(error): error output management

--- Basics - Core concept/08-cmp-advanced/src/components/AuthorInfo.vue
+++ Basics - Core concept/08-cmp-advanced/src/components/AuthorInfo.vue
@@ -7,17 +7,42 @@
         <base-badge type="author" caption="AUTHOR"></base-badge>
       </template>
       <p>WRITING is mine</p>
+      <input type="text" ref="inputText" />
+      <button @click="submitPush">POUSSEZ</button>
+      <Error v-if="inputIsInvalid">
+        <h2>input cannot be empty</h2>
+        <p>Please provide some content</p>
+        <button @click="retry">RETRY</button>
+      </Error>
     </BaseCard>
   </section>
 </template>
 
 <script>
 import BaseCard from './BaseCard.vue';
+import Error from './Error.vue';
 
 export default {
   components: {
     BaseCard,
+    Error,
   },
   props: ['fullName', 'infoText', 'role'],
+  data() {
+    return {
+      inputText: '',
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    submitPush() {
+      if (this.$refs.inputText.value === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    retry() {
+      this.inputIsInvalid = false;
+    },
+  },
 };
 </script>
