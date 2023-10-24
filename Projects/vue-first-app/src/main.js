a433af3184617cb3aa37f9f67af3e89f89c4b20c
Cyril Marois <cyrilmarois@gmail.com>
2023-10-24 14:20:52 +0200

    feat(form): set up cmpt

--- Projects/vue-first-app/src/main.js
+++ Projects/vue-first-app/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import ContactList from './components/ContactList.vue';
+import ContactFormVue from './components/ContactForm.vue';
 
 const app = createApp(App);
 
 app.component('contact-list', ContactList);
+app.component('contact-form', ContactFormVue);
 app.mount('#app');
