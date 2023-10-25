af60359783512ff89e9876344333a22fd8ef9117
Cyril Marois <cyrilmarois@gmail.com>
2023-10-25 09:59:02 +0200

    feat(app): dummy output

---
+++ Projects/learning/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul>
+    <li v-for="storedResource in storedResources" :key="storedResource.id">
+      <p>
+        <span>Title</span>: <span>{{ storedResource.title }}</span>
+      </p>
+      <p>
+        <span>Description</span>: <span>{{ storedResource.description }}</span>
+      </p>
+      <p>
+        <span>Resource</span>:
+        <span>
+          <a :href="storedResource.link">{{ storedResource.link }}</a>
+        </span>
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js document',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn Google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
