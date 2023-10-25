b21cb06b93fd1be58d8deb0e6423068aaf932997
Cyril Marois <cyrilmarois@gmail.com>
2023-10-26 00:15:06 +0200

    feat(auth): create store auth module

--- Basics - Core concept/09-auth-starting-stup/src/store/index.js
+++ Basics - Core concept/09-auth-starting-stup/src/store/index.js
@@ -2,22 +2,23 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import AuthModule from './modules/auth/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestsModule
+    requests: requestsModule,
   },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
-  }
+  // state() {
+  //   return {
+  //     userId: 'c3',
+  //   };
+  // },
+  // getters: {
+  //   userId(state) {
+  //     return state.userId;
+  //   },
+  // },
 });
 
 export default store;
