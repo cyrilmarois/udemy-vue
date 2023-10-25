5aa0618528f974a5c499ec82518a95654c65d047
Cyril Marois <cyrilmarois@gmail.com>
2023-10-26 00:26:25 +0200

    feat(auth):  firebase authentication module

--- Basics - Core concept/09-auth-starting-stup/src/store/modules/auth/actions.js
+++ Basics - Core concept/09-auth-starting-stup/src/store/modules/auth/actions.js
@@ -1,1 +1,31 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_7nMbeNJz4tG8FHtTqqzA_akLhFRMQc4',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    if (!response.status !== 200) {
+      const error = new Error(responseData.message || 'Authentication failed');
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
