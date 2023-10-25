08109b78ac400c9abda70beedaaa541c876459de
Cyril Marois <cyrilmarois@gmail.com>
2023-10-25 19:24:16 +0200

    feat(data): http get request to firebase DB

--- Basics - Core concept/08-http-starting-setup/src/components/survey/UserExperiences.vue
+++ Basics - Core concept/08-http-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="getExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -18,12 +20,25 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    async getExperiences() {
+      const res = await axios.get(
+        'https://vue-htt-demo-c141a-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
+      );
+      this.results = res.data;
+    },
   },
 };
 </script>
