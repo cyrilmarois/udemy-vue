87049002a5e85cadcd443b9df96fd59675d7cb3a
Cyril Marois <cyrilmarois@gmail.com>
2023-10-26 10:04:22 +0200

    word(module): fix typo

--- Basics - Core concept/09-auth-starting-stup/src/router.js
+++ Basics - Core concept/09-auth-starting-stup/src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
@@ -24,7 +24,7 @@ const router = createRouter({
     },
     {
       path: '/register',
-      component: CoachRegistation,
+      component: CoachRegistration,
       meta: { requireAuth: true },
     },
     { path: '/auth', component: UserAuth, meta: { requireUnauth: true } },
