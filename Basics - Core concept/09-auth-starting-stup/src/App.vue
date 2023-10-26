78775fdf8bf3eba185b838a64f937f9323299ad4
Cyril Marois <cyrilmarois@gmail.com>
2023-10-26 09:10:43 +0200

    feat(data): insert dummy data

--- Basics - Core concept/09-auth-starting-stup/src/App.vue
+++ Basics - Core concept/09-auth-starting-stup/src/App.vue
@@ -8,24 +8,38 @@
 </template>
 
 <script>
+// import axios from 'axios';
 import TheHeader from './components/layout/TheHeader.vue';
 
 export default {
   components: {
-    TheHeader
-  }  
-}
+    TheHeader,
+  },
+  beforeMount() {
+    // axios.post(
+    //   'https://vue-htt-demo-c141a-default-rtdb.europe-west1.firebasedatabase.app/coaches.json',
+    //   {
+    //     id: 'c2',
+    //     firstName: 'Angie',
+    //     lastName: 'Doey',
+    //     areas: ['frontend', 'career'],
+    //     description: "I'm Angie and I love cat!",
+    //     hourlyRate: 66,
+    //   }
+    // );
+  },
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
