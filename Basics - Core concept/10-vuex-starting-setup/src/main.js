2f911e149a19b5b45e7aa4cf5a2a3dd6a1147bf4
Cyril Marois <cyrilmarois@gmail.com>
2023-11-02 17:34:16 +0100

    feat(getters): transform finalCounter getters

--- Basics - Core concept/10-vuex-starting-setup/src/main.js
+++ Basics - Core concept/10-vuex-starting-setup/src/main.js
@@ -22,6 +22,16 @@ const store = createStore({
     finalCounter(state) {
       return state.counter;
     },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
   },
 });
 app.use(store);
