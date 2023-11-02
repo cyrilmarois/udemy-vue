5071b377cf8852aae34573bfb578d308bdb53065
Cyril Marois <cyrilmarois@gmail.com>
2023-11-02 18:09:47 +0100

    feat(mapActions): playing with mapActions

--- Basics - Core concept/10-vuex-starting-setup/src/components/TheChangeCounter.vue
+++ Basics - Core concept/10-vuex-starting-setup/src/components/TheChangeCounter.vue
@@ -1,13 +1,17 @@
 <template>
   <button @click="addTen">add 10</button>
+  <button @click="increment10({ value: 11 })">add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   methods: {
     addTen() {
       this.$store.dispatch('increment10', { value: 10 });
     },
+    ...mapActions(['increment10']),
   },
 };
 </script>
