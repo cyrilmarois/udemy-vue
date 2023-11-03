5140fc3fb9697b69b0cf837b017578725cb33cfd
Cyril Marois <cyrilmarois@gmail.com>
2023-11-04 00:22:26 +0100

    feat(router): load components according to route

--- Basics - Core concept/12-routing-starting-setup/src/main.js
+++ Basics - Core concept/12-routing-starting-setup/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import TheNotFound from './components/TheNotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
@@ -16,7 +18,10 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter,
+      },
       children: [
         {
           name: 'team-members',
@@ -28,7 +33,10 @@ const router = createRouter({
     },
     {
       path: '/users',
-      component: UsersList,
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
     },
     { path: '/:notFound(.*)', component: TheNotFound },
   ],
