61b1d8cf6cf2776e373af21fd12e5633b471f85d
Cyril Marois <cyrilmarois@gmail.com>
2023-11-07 15:10:28 +0100

    feat(router): load app when ready

--- Basics - Core concept/13-animations-starting-setup/src/main.js
+++ Basics - Core concept/13-animations-starting-setup/src/main.js
@@ -5,7 +5,9 @@ import BaseModal from './components/BaseModal.vue';
 import router from './router.js';
 
 const app = createApp(App);
-app.use(router);
 app.component('base-modal', BaseModal);
 
-app.mount('#app');
+app.use(router);
+router.isReady().then(() => {
+  app.mount('#app');
+});
