ae29377d650aa9de288fc04738e21b8e07e44d23
Cyril Marois <cyrilmarois@gmail.com>
2023-11-07 08:28:41 +0100

    feat(transition): animation of block

--- Basics - Core concept/13-animations-starting-setup/src/App.vue
+++ Basics - Core concept/13-animations-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{ animate: animateBlock }"></div>
+    <button @click="animate">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,12 +10,12 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { animateBlock: false, dialogIsVisible: false };
   },
   methods: {
     showDialog() {
@@ -23,6 +23,9 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    animate() {
+      this.animateBlock = true;
     },
   },
 };
@@ -57,6 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /* transition: transform 500ms ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -68,5 +72,21 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  animation: smurf 500ms ease-out forwards;
+}
+@keyframes smurf {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
