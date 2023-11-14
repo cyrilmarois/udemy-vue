eeda2d237201db87970b46ccebb248f06f226af4
Cyril Marois <cyrilmarois@gmail.com>
2023-11-14 09:24:57 +0100

    feat(composition): ref / reactive

--- Basics - Core concept/14-composition-starting-setup/src/App.vue
+++ Basics - Core concept/14-composition-starting-setup/src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <div>
+      <h2>{{ userA.name }}</h2>
+      <h3> {{ userA.age }}</h3>
+    </div>
+    <div>
+      <h2>{{ userB.name }}</h2>
+      <h3> {{ userB.age }}</h3>
+    </div>
   </section>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
 
-const userName = ref( 'Maximilian' );
+// const userName = ref( 'Maximilian' );
+// const age = ref( 37 );
+
+const userA = ref( {
+  name: 'Max',
+  age: 31
+} );
+
+const userB = reactive( {
+  name: 'Michel',
+  age: 36,
+} );
 
 setTimeout( () => {
-  userName.value = 'Cyril';
+  userA.value.name = 'Cyril';
+  userA.value.age = 37;
+
+  userB.name = 'Lauren';
+  userB.age = 37;
 }, 2000 );
 
 </script >
