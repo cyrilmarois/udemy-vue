178beb4e082212449b22a41b91d178ace7875874
Cyril Marois <cyrilmarois@gmail.com>
2023-11-15 22:37:59 +0100

    feat(routes): Link routes to components

--- Projects/Coach-finder/src/router.js
+++ Projects/Coach-finder/src/router.js
@@ -1,25 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import coachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { name: 'home', path: '/', redirect: '/coaches' },
-    { name: 'coaches', path: '/coaches', component: null },
+    { name: 'coaches', path: '/coaches', component: coachesList },
     {
       name: 'coachDetails',
       path: '/coaches/:id',
-      component: null,
+      component: CoachDetails,
       children: [
         {
           name: 'contact',
-          path: '/contact', // coaches/c1/contact
-          component: null,
+          path: 'contact', // coaches/c1/contact
+          component: ContactCoach,
         },
       ],
     },
-    { name: 'registerCoach', path: '/register', component: null },
-    { name: 'requests', path: '/requests', component: null },
-    { name: 'notFound', path: '/:notFound(.*+)', component: null },
+    { name: 'registerCoach', path: '/register', component: CoachRegistration },
+    { name: 'requests', path: '/requests', component: RequestsReceived },
+    { name: 'notFound', path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
