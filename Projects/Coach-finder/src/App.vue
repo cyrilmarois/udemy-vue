20ee5e50c806a61e1b5588c2f745f55b150cc1f7
Cyril Marois <cyrilmarois@gmail.com>
2023-11-15 22:48:45 +0100

    feat(nav):  Basic header with nav to caoches/requests

--- Projects/Coach-finder/src/App.vue
+++ Projects/Coach-finder/src/App.vue
@@ -1,7 +1,29 @@
 <template>
-    <div></div>
+    <div>
+        <TheHeader />
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {}
+import TheHeader from './components/layout/TheHeader.vue';
+export default {
+    components: {TheHeader}
+}
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: "Roboto", sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
--- Projects/Coach-finder/src/pages/coaches/CoachesList.vue
+++ Projects/Coach-finder/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div>FILTER LIST OF COACHES</div>
 </template>
 
 <script></script>
