035b4c00dddd3240e6568eaf9ab759412bd89ab5
Cyril Marois <cyrilmarois@gmail.com>
2023-11-20 17:26:43 +0100

    feat(requests): register request to a coach

--- Projects/Coach-finder/src/pages/requests/RequestsReceived.vue
+++ Projects/Coach-finder/src/pages/requests/RequestsReceived.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>COACH MESSAGES</div>
+    <section>
+        <base-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+            <ul v-if="hasRequests">
+                <li v-for="request in receivedRequests" :key="request.id">
+                    <RequestItem :email="request.userEmail" :message="request.userMessage" />
+                </li>
+            </ul>
+            <h3 v-else>No request yet</h3>
+        </base-card>
+
+    </section>
 </template>
 
-<script></script>
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+    components: {RequestItem},
+    computed: {
+        receivedRequests() {
+            console.table( this.$store.getters[ 'requests/requests' ] )
+            return this.$store.getters[ 'requests/requests' ];
+        },
+        hasRequests() {
+            return this.$store.getters[ 'requests/hasRequests' ];
+        }
+    }
+}
+</script>
+<style scoped>
+header {
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
+</style>
--- Projects/Coach-finder/src/store/modules/requests/getters.js
+++ Projects/Coach-finder/src/store/modules/requests/getters.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  requests(state) {
+    return state.requests;
+  },
+  hasRequests(_, getters) {
+    return getters?.requests.length > 0;
+  },
+};
