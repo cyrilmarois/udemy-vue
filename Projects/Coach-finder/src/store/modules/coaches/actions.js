5b429d13d87ae243367493cb626388fd37fe8bfc
Cyril Marois <cyrilmarois@gmail.com>
2023-11-20 17:59:35 +0100

    feat(coach): register a coach to DB

--- Projects/Coach-finder/src/store/modules/coaches/actions.js
+++ Projects/Coach-finder/src/store/modules/coaches/actions.js
@@ -1,7 +1,20 @@
-export default {
-  registerCoach(context, payload) {
-    payload = { ...payload, id: context.rootGetters.userId };
+const BASE_URL = process.env.VUE_APP_API_BASE_URL;
 
-    context.commit('registerCoach', payload);
+export default {
+  async registerCoach(context, payload) {
+    const coachId = context.rootGetters.userId;
+
+    payload = { ...payload };
+    try {
+      const response = await fetch(`${BASE_URL}/coaches/${coachId}.json`, {
+        method: 'PUT',
+        body: JSON.stringify(payload),
+      });
+      console.table({ response });
+    } catch (e) {
+      throw new Error(e.message);
+    }
+
+    context.commit('registerCoach', { ...payload, id: coachId });
   },
 };
