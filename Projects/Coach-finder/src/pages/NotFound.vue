603299c9f0711f9d24efb1c16e059c51e9805bae
Cyril Marois <cyrilmarois@gmail.com>
2023-11-21 10:27:01 +0100

    feat(not found): not found page

--- Projects/Coach-finder/src/pages/NotFound.vue
+++ Projects/Coach-finder/src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-    <div></div>
+    <div>
+        <section>
+            <base-card>
+                <h2>Page not found</h2>
+                <p>There is nothing here ! Please visit our <router-link to="/coaches"> coaches </router-link> !</p>
+            </base-card>
+        </section>
+
+    </div>
 </template>
 
 <script></script>
